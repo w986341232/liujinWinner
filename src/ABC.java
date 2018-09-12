8ff49ea9ca54626a0c32b0446f8210464b8f2eb2
liuijn <17312838102@163.com>
2018-09-12 22:52:50 +0800

    123

--- src/ABC.java
+++ src/ABC.java
@@ -1,7 +1,7 @@
 
 public class ABC {
 	public static void main(String[] args) {
-		System.out.println("你好，我是小胡");
-		System.out.println("小刘改了");
+		System.out.println("123");
+		
 	}
 }
--- src/T.java
+++ src/T.java
@@ -2,9 +2,6 @@
 public class T {
 
 	public static void main(String[] args) {
-		System.out.println("111111");
-		System.out.println("22");
-		
 	}
 
 }
