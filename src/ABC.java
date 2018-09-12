9d03dc6192ec20260cf65ad0f811e27488c72c6f
liuijn <17312838102@163.com>
2018-09-12 22:39:16 +0800

    1

--- src/ABC.java
+++ src/ABC.java
@@ -3,7 +3,7 @@ public class ABC {
 	public static void main(String[] args) {
 		System.out.println("你好，我是小胡");
 		System.out.println("小刘改了");
-		
+		System.err.println("222222222222222222222222");
 		System.out.println("测试代码！！！！！");
 	}
 }
